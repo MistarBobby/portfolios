14820c5677dd033a4aa41a0e7fcba50eb9248cac
Olasunkanmi Bayode <bobbyninjay@mail.com>
2024-07-13 00:20:48 +0100

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function toggleMenu() {
+/* function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
     
+} */
+
+
+function toggleMenu() {
+    const menu = document.querySelector('.hamburger-menu .menu-links');
+    menu.classList.toggle('active');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.hamburger-menu .menu-links a');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            toggleMenu();
+        });
+    });
+});
